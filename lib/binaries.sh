40fdfea1aa2bd730e8a563c662437af5690cb2c0
Jacques Raguin <jacques.raguin@beta.gouv.fr>
2023-01-06 18:12:57 +0100

    Bricole l'installation de node

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -103,7 +103,32 @@ install_nodejs() {
     if [[ "$resolve_result" == "failed" ]]; then
       fail_bin_install node "$version"
     fi
-
+    ARCH= && dpkgArch="$(dpkg --print-architecture)"
+    case "${dpkgArch##*-}" in 
+      amd64) ARCH='x64';; 
+      ppc64el) ARCH='ppc64le';; 
+      s390x) ARCH='s390x';;
+      arm64) ARCH='arm64';;
+      armhf) ARCH='armv7l';;
+      i386) ARCH='x86';;
+    *) echo "unsupported architecture"; exit 1 ;;
+  esac
+  # gpg keys listed at https://github.com/nodejs/node#release-keys
+  set -ex
+  for key in 4ED778F539E3634C779C87C6D7062848A1AB005C141F07595B7B3FFE74309A937405533BE57C7D5774F12602B6F1C4E913FAA37AD3A89613643B620161FC681DFB92A079F1685E77973F295594EC46898FCCA13FEF1D0C2E91008E09770F7A9A5AE15600C4F0DFFF4E8C1A8236409D08E73BC641CC11F4C8890C08DB8579162FEE0DF9DB8BEAB4DFCF555EF4C82FA3AE1CBEDC6BE46B9360C43CEC45C17AB93C108F52B48DB57BB0CC439B2997B01419BD92F80A; do 
+      gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys "$key" ||
+      gpg --batch --keyserver keyserver.ubuntu.com --recv-keys "$key" ; 
+  done 
+  curl -fsSLO --compressed "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-$ARCH.tar.xz" 
+  curl -fsSLO --compressed "https://nodejs.org/dist/v$NODE_VERSION/SHASUMS256.txt.asc" 
+  gpg --batch --decrypt --output SHASUMS256.txt SHASUMS256.txt.asc 
+  grep " node-v$NODE_VERSION-linux-$ARCH.tar.xz\$" SHASUMS256.txt | sha256sum -c - 
+  tar -xJf "node-v$NODE_VERSION-linux-$ARCH.tar.xz" -C /usr/local --strip-components=1 --no-same-owner 
+  rm "node-v$NODE_VERSION-linux-$ARCH.tar.xz" SHASUMS256.txt.asc SHASUMS256.txt 
+  ln -s /usr/local/bin/node /usr/local/bin/nodejs 
+  # smoke tests
+  node --version 
+  npm --version
     echo "Downloading and installing node $number..."
   fi
 
